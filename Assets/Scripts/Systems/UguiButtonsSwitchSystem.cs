a0e3269ee20d7ff7b125737c2a04f2ec20384125
Fenneig <ninetailedkot92@gmail.com>
2023-03-23 19:01:17 +0300

    add button state switch logic

---
+++ Assets/Scripts/Systems/UguiButtonsSwitchSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using EcsSudoku.Components;
+using EcsSudoku.Services;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+
+namespace EcsSudoku.Systems
+{
+    public class UguiButtonsSwitchSystem : IEcsInitSystem, IEcsRunSystem
+    {
+        private readonly EcsFilterInject<Inc<CellClickedEvent>> _filter = Idents.Worlds.Events;
+        private readonly EcsFilterInject<Inc<Number>> _numberFilter = default;
+        
+        private readonly EcsCustomInject<Configuration> _config = default;
+        private readonly EcsCustomInject<SceneData> _sceneData = default;
+
+        private Dictionary<int, int> _fieldNumbersCount;
+
+        public void Init(IEcsSystems systems)
+        {
+            _fieldNumbersCount = new Dictionary<int, int>(_config.Value.GridSize);
+            for (int i = 0; i < _config.Value.GridSize; i++)
+            {
+                _fieldNumbersCount[i + 1] = 0;
+            }
+        }
+
+        public void Run(IEcsSystems systems)
+        {
+            foreach (var _ in _filter.Value)
+            {
+                for (int i = 0; i < _config.Value.GridSize; i++)
+                {
+                    _fieldNumbersCount[i + 1] = 0;
+                }
+                
+                foreach (var entity in _numberFilter.Value)
+                {
+                    if (_numberFilter.Pools.Inc1.Get(entity).Value ==0) continue;
+                    _fieldNumbersCount[_numberFilter.Pools.Inc1.Get(entity).Value]++;
+                }
+                
+                foreach (var i in _fieldNumbersCount)
+                {
+                    _sceneData.Value.NumberButtons[i.Key - 1].SetActive(i.Value < 9);
+                } 
+            }
+        }
+    }
+}
